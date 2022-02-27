69b7bc31d2b3d043644e88cb134c4853f934b45e
IsacBorjesson <isac.borjeson99@gmail.com>
2022-02-27 20:36:34 +0100

    Fixed the checkout, and added bugd form

--- js/script.js
+++ js/script.js
@@ -2,19 +2,56 @@ const rankingsBody = document.querySelector("#rankings-table > tbody");
 const test = document.querySelector("#myItemList > ul");
 
 const array = []
-function loadRankings() {
+function loadCourses() {
     const request = new XMLHttpRequest();
 
     request.open("get", "../data/rankings.json");
     request.onload = () => {
         const json = JSON.parse(request.responseText);
-        editRankings(json);
-        populateRanklings(json);
+        addToCheckout(json);
+        populateCourses(json);
+        addToCourses(json)
     }
     request.send();
+    
 };
 
-function populateRanklings(json) {
+function addToCourses(json){
+    var id
+    var name
+    var course
+    var email
+    
+    document.querySelectorAll(".content").forEach(inputElement => {
+        inputElement.addEventListener("blur", e => {
+            if (e.target.id === "new_Id" && e.target.value.length > 0 && e.target.value >= 0) {
+                id = e.target.value
+            }
+            if (e.target.id === "new_Name" && e.target.value.length > 0) {
+                name = e.target.value
+            }
+            if (e.target.id === "new_Course" && e.target.value.length > 0) {
+                course = e.target.value
+            }
+            if (e.target.id === "new_Email" && e.target.value.length > 0) {
+                email = e.target.value
+            }
+            if(id != null && name != null && course != null && email != null){
+                document.addEventListener("submit", e => {
+                    e.preventDefault();
+                    var newCourse = [id,name, course, email]
+                    json.push(newCourse)
+                    populateCourses(json)
+                });
+                
+            }
+        })
+    })
+}
+     
+
+
+function populateCourses(json) {
 
     // Clear out existing table data
     while (rankingsBody.firstChild) {
@@ -34,37 +71,41 @@ function populateRanklings(json) {
         rankingsBody.appendChild(tr);
 
     });
+   
 }
 
-function populateCheckout(array) {
-    var test = document.getElementById("myItemList");
 
+function populateCheckout() {
+    var test = document.getElementById("myItemList");
+    var index = 0
+    // Clear out existing table data
     while (test.firstChild)
         test.removeChild(test.firstChild);
 
-    let items = [
-        'Blue',
-        'Red',
-        'White',
-        'Green',
-        'Black',
-        'Orange'
-    ],
     ul = document.createElement('ul');
 
 
     document.getElementById('myItemList').appendChild(ul);
 
-
+    
     array.forEach(function (item) {
         let li = document.createElement('li');
         ul.appendChild(li);
-
-        li.innerHTML += item;
+        li.innerHTML += item + " " + `<button onclick="myFunction(${index})"">Delete</button>` ;
+        index++
     });
+    
+}
+function myFunction(index){
+    console.log(index)
+    array.splice(index,1)
+    populateCheckout()
 }
 
-function editRankings(json){
+
+    
+
+function addToCheckout(json){
     console.log(json)
     var searchId 
     
@@ -74,10 +115,12 @@ function editRankings(json){
             searchId = e.target.value;
             console.log(searchId)
             array.push(json[searchId][2])
-            populateCheckout(array);
+            populateCheckout();
         }
     })
 })
 
 }
-document.addEventListener("DOMContentLoaded", () => { loadRankings(); });
+document.addEventListener("DOMContentLoaded", () => { loadCourses(); });
+
+
